0095f9e6c4a8f3f90112d910dd6f4e4053aa07d6
fengyj <fyj@fyj-ThinkPad-E485>
2020-04-26 13:30:00 +0800

    数组,链表结构

---
+++ AlgorithmTest/src/main/java/com/fyj/myArray/MyArray.java
@@ -0,0 +1,84 @@
+package com.fyj.myArray;
+
+/**
+ * @author fyj
+ */
+public class MyArray {
+    private int[] array;
+    private int size;
+
+    public MyArray(int capacity){
+       this.array=new int[capacity];
+       size = 0;
+    }
+
+    /**
+     * 插入
+     * @param element 数组元素
+     * @param index  下标
+     * @throws Exception
+     */
+    public void insert(int element,int index) throws Exception{
+        if(index < 0 || index > size){
+          throw  new IndexOutOfBoundsException("超出数组的实际元素范围!");
+        }
+
+        //如果实际元素达到数组容量上限，则对数组进行扩容
+        if(size >= array.length){
+            resize();
+        }
+
+        //从右向左循环，将元素逐个向右挪一位
+        for(int i=size-1;i>=index;i--){
+           array[i+1]=array[i];
+        }
+        //插入元素
+        array[index] = element;
+        size++;
+    }
+
+    /**
+     * 删除元素
+     * @param index
+     * @return
+     * @throws Exception
+     */
+    public int delete(int index) throws Exception{
+        if(index <0 || index >=size){
+            throw new IndexOutOfBoundsException("超出数组的实际元素范围");
+        }
+        int deletedElement = array[index];
+        for(int i=index;i<size-1;i++){
+           array[i]=array[i+1];
+        }
+        size--;
+        return deletedElement;
+    }
+
+    public void output(){
+        for(int i = 0;i<size;i++){
+            System.out.println(array[i]);
+        }
+    }
+    /**
+     * 数组扩容
+     */
+    public void resize(){
+        int[] arrayNew = new int[array.length*2];
+        System.arraycopy(array,0,arrayNew,0,array.length);
+        array = arrayNew;
+    }
+
+    public static void main(String[] args){
+        try {
+            MyArray myArray = new MyArray(2);
+            myArray.insert(1, 0);
+            myArray.insert(2,1);
+            myArray.insert(3,2);
+            myArray.output();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+    }
+}
---
+++ AlgorithmTest/src/main/java/com/fyj/myLinkedList/SigleLinkedList.java
@@ -0,0 +1,136 @@
+package com.fyj.myLinkedList;
+
+/**
+ * @author fyj
+ */
+public class SigleLinkedList {
+
+    //头节点指针
+    private Node head;
+
+    //尾节点指针
+    private Node last;
+
+    //链表长度
+    private int size;
+
+
+    /**
+     * 链表插入元素
+     * @param data
+     * @param index
+     * @throws Exception
+     */
+    public void insert(int data,int index) throws  Exception{
+        if(index <0 || index >size){
+            throw new IndexOutOfBoundsException("超过链表元素节点范围");
+        }
+        Node insertedNode = new Node(data);
+        if(size == 0){
+            //空链表
+            head = insertedNode;
+            last = insertedNode;
+        }else if(0==index){
+            //插入头节点
+            insertedNode.next=head;
+            head = insertedNode;
+        }else if(size==index){
+            //插入尾部
+           last.next=insertedNode;
+           last = insertedNode;
+        }else{
+            //插入中间
+            Node preNode = get(index-1);
+            insertedNode.next=preNode.next;
+            preNode.next=insertedNode;
+        }
+
+        size++;
+    }
+
+    /**
+     * 删除链表元素
+     * @param index
+     * @return
+     * @throws Exception
+     */
+    public Node delete(int index) throws  Exception{
+        if(index <0||index >=size){
+           throw  new IndexOutOfBoundsException("超过链表节点范围");
+        }
+        Node deletedNode = null;
+        if(index==0){
+            //删除头节点
+            deletedNode = head;
+            head = head.next;
+        }else if(index == size-1){
+            //删除尾节点
+            deletedNode = last;
+            Node preNode = get(index-1);
+            preNode.next = null;
+            last = preNode;
+        }else{
+            //删除中间节点
+            Node preNode = get(index-1);
+            deletedNode = preNode.next;
+            preNode.next = preNode.next.next;
+        }
+
+        size--;
+        return deletedNode;
+    }
+    /**
+     * 链表查找元素
+     * @param index 查找的位置
+     * @return
+     * @throws Exception
+     */
+    public Node get(int index) throws  Exception{
+
+        if(index <0 || index >= size){
+            throw  new IndexOutOfBoundsException("超过链表元素节点范围");
+        }
+        Node temp = head;
+        for(int i=0;i<index;i++){
+            temp = temp.next;
+        }
+        return temp;
+    }
+
+
+    /**
+     * 输出链表
+     */
+    public void output(){
+        Node temp = head;
+        while(temp != null){
+            System.out.println(temp.data);
+            temp = temp.next;
+        }
+    }
+
+
+    public static void main(String[] args){
+        try {
+            SigleLinkedList sigleLinkedList = new SigleLinkedList();
+            sigleLinkedList.insert(1, 0);
+            sigleLinkedList.insert(2,1);
+            sigleLinkedList.output();
+            sigleLinkedList.delete(0);
+            sigleLinkedList.output();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+
+
+
+    private static class Node{
+        int data;
+        Node next;
+        Node(int data){
+         this.data=data;
+        }
+    }
+}
