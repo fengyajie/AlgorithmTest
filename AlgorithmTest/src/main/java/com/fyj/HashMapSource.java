e12173726b1876d9dbe10b2b97833f0324e2b948
fengyj <fyj@fyj-ThinkPad-E485>
2020-05-19 20:32:45 +0800

    hashMap源码

---
+++ AlgorithmTest/src/main/java/com/fyj/HashMapSource.java
@@ -0,0 +1,11 @@
+package com.fyj;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author fyj
+ */
+public class HashMapSource {
+   Map<String,Object> map =  new HashMap<String,Object>();
+}
